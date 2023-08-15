7b9b76b8a85adb55ba6bf38bf700020076c9ce09
delsftr <dellasafitri82@gmail.com>
2023-08-15 13:12:16 +0700

    update agustus

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,20 +1,20 @@
 <!--NAVBAR DESKTOP-->
 <nav class="side-nav nav-container">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#homepage">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">
             <img src="<?php echo base_url('assets/img/tricom.png'); ?>" alt="Tricom" width="60" height="auto" />
         </a>
     </div>
     <div class="side-nav">
         <ul class="nav-menu p-0">
             <li class="nav-item">
-                <a href="#about-us" class="text-decoration-none"><i class="about-us"></i><span class="menu-text text-dark">About Us</span></a>
+                <a href="<?= base_url("#about-us"); ?>" class="text-decoration-none"><i class="about-us"></i><span class="menu-text text-dark">About Us</span></a>
             </li>
             <li class="nav-item">
-                <a href="#services" class="text-decoration-none"><i class="services"></i><span class="menu-text text-dark">Services</span></a>
+                <a href="<?= base_url("#services"); ?>" class="text-decoration-none"><i class="services"></i><span class="menu-text text-dark">Services</span></a>
             </li>
             <li class="nav-item">
-                <a href="#contact" class="text-decoration-none"><i class="contact"></i><span class="menu-text text-dark">Contact</span></a>
+                <a href="<?= base_url("#contact"); ?>" class="text-decoration-none"><i class="contact"></i><span class="menu-text text-dark">Contact</span></a>
             </li>
         </ul>
     </div>
@@ -23,7 +23,7 @@
 <!--NAVBAR TABLET & MOBILE-->
 <nav class="top-nav navbar bg-light sticky-top shadow-lg">
     <div class="container px-4">
-        <a class="navbar-brand" href="#homepage"><img src="<?php echo base_url('assets/img/tricom.png'); ?>" alt="Tricom" width="60" height="auto" /></a>
+        <a class="navbar-brand" href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/img/tricom.png'); ?>" alt="Tricom" width="60" height="auto" /></a>
         <button class="navbar-toggler border-0" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <i class="bi bi-list" style="color: #0659a7; font-size: 2rem; margin-right: -1rem"></i>
         </button>
@@ -33,7 +33,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="navbar-nav offcanvas-body text">
-                <a class="nav-link my-3" style="padding-top: 1.5rem; border-top-width: 1px; border-top-style: solid; border-top-color: #0659a7" href="#about-us">About Us</a>
+                <a class="nav-link my-3" style="padding-top: 1.5rem; border-top-width: 1px; border-top-style: solid; border-top-color: #0659a7" href="<?= base_url("#about-us"); ?>">About Us</a>
                 <a class="nav-link dropdown-toggle my-3" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Services </a>
                 <ul class="dropdown-menu text">
                     <a class="dropdown-item" href="/social-media">Social Media</a>
@@ -41,7 +41,7 @@
                     <a class="dropdown-item" href="/video-production">Video Production</a>
                     <a class="dropdown-item" href="/web-design">Web Design</a>
                 </ul>
-                <a class="nav-link my-3" href="#contact">Contact</a>
+                <a class="nav-link my-3" href="<?= base_url("#contact"); ?>">Contact</a>
             </div>
             <!-- <div class="offcanvas-sosmed mx-5 m-5" style="margin-bottom: 5rem !important">
                 <p class="text fw-semibold">Follow Us</p>
@@ -52,3 +52,19 @@
     </div>
 </nav>
 <!-- NAVBAR END-->
+
+<!-- NAVBAR PAGE-->
+<!-- <nav class="side-nav nav-container">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">
+            <img src="<?php echo base_url('assets/img/tricom.png'); ?>" alt="Tricom" width="60" height="auto" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="nav-icon text-center" id="navbarIcon">
+        <a href="<?php echo base_url(); ?>"><i class="bi bi-arrow-left" style="font-size: 30px; color: #000000"></i></a>
+    </div>
+</nav> -->
+<!-- NAVBAR PAGE END -->
